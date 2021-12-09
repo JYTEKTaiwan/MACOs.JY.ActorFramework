bdbbe1783e29dfb7e6340e97ef6e9abfd0e5d56a
JYTEK Taiwan <54968659+JYTEKTaiwan@users.noreply.github.com>
2021-12-09 10:48:43 +0800

    Revert "Delete unit test/net472_UnitTest directory"
    This reverts commit 5fd396f6c3cacb3523eb50182993e707b1b6daa1.

---
+++ unit test/net472_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("net472_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("net472_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("212e65e8-a3a1-4d60-8730-6f25f77e0cbb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
